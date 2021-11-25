c3b648ca20c3d25517365d4c33d2db3a04b3e747
MinhTung-99 <58562847+MinhTung-99@users.noreply.github.com>
2021-11-25 16:53:32 +0700

    handle sign up

---
+++ app/src/main/java/com/example/androidkotlinchaning/view/CustomEdittextAuthentication.kt
@@ -0,0 +1,9 @@
+package com.example.androidkotlinchaning.view
+
+import android.content.Context
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
+
+class CustomEdittextAuthentication(context: Context) : AppCompatEditText(context) {
+
+}
