49c0a0743a95407bbf69b1efb5ce7eeb2eaebcfb
minhtung-99 <tung07011999@gmail.com>
2021-12-15 22:43:36 +0700

    ui main

--- app/src/main/java/com/example/androidkotlinchaning/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinchaning/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.androidkotlinchaning
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions.circleCropTransform
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
+import com.example.androidkotlinchaning.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val menu = binding.bottomNavigationView.menu
+        val menuItem = menu.findItem(R.id.menu_profile)
+        Glide.with(this)
+            .asBitmap()
+            .load("https://cdn.tgdd.vn/Files/2020/06/08/1261696/moi-tai-bo-hinh-nen-asus-rog-2020-moi-nhat_800x450.jpg")
+            .apply(
+                circleCropTransform()
+                    .placeholder(R.drawable.ic_personal)
+            )
+        .into(object : SimpleTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                menuItem?.icon = BitmapDrawable(resources, resource)
+            }
+        })
     }
 }
